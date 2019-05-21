aeff695355947a252ce4b71a6fb3892884c5abf4
Yaroslav Lysenko <lysenko.yaroslav.kharkov@gmail.com>
2019-05-21 08:04:33 +0300

    added vagrant user to vboxsf group

--- centOS_instance.sh
+++ centOS_instance.sh
@@ -4,5 +4,7 @@ sudo yum update -y;
 sudo yum install mc -y;
 sudo yum install wget -y;
 
+sudo usermod -aG vboxsf vagrant;
+
 
 echo -e "********* DONE *********"
--- ubuntu_instance.sh
+++ ubuntu_instance.sh
@@ -4,5 +4,7 @@ sudo apt-get update;
 sudo apt-get upgrade -y;
 sudo apt-get install mc -y;
 
+sudo usermod -aG vboxsf vagrant;
+
 
 echo -e "********* DONE *********"
