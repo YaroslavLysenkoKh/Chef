f236985278fde23e36a40f9ec2a2cf566554ec16
Yaroslav Lysenko <lysenko.yaroslav.kharkov@gmail.com>
2019-06-08 01:10:12 +0300

    php recipe added

---
+++ chef-repo/cookbooks/lamp/recipes/php.rb
@@ -0,0 +1,16 @@
+package 'yum-utils' do
+  action :install
+end
+
+execute 'php_version' do
+  command 'yum-config-manager --enable remi-php56.repo '
+  ignore_failure true
+end
+
+%w{php php-fpm php-mysql php-xmlrpc php-gd php-pear php-pspell}.each do |pkg|
+  package pkg do
+    flush_cache before: true
+    action :install
+    notifies :reload, 'service[httpd]', :immediately
+  end
+end
