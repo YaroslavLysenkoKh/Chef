b1d1f83054072f91f45b3984727223eb7d964edd
Yaroslav Lysenko <lysenko.yaroslav.kharkov@gmail.com>
2019-06-08 12:58:00 +0300

    default recipe updated.

--- chef-repo/cookbooks/lamp/recipes/default.rb
+++ chef-repo/cookbooks/lamp/recipes/default.rb
@@ -1,5 +1,4 @@
-#
-# Cookbook:: lamp
-# Recipe:: default
-#
-# Copyright:: 2019, The Authors, All Rights Reserved.
+include_recipe 'lamp::apache'
+include_recipe 'lamp::php'
+include_recipe 'lamp::mysql'
+include_recipe 'lamp::wordpress'
